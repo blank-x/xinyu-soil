4db780c74a19afecbc744c976d02b303798e7849
zhaoshaoyong1970 <zhaoshaoyong1970@foxmail.com>
2023-06-26 21:56:32 +0800

    add

---
+++ packages/eslint-plugin-forbid-global/src/index.js
@@ -0,0 +1,20 @@
+module.exports.rules = {
+  'no-global': require('./no-global'),
+}
+module.exports.processors = {
+  meta: {
+    name: "no-globals",  
+    description: "禁止使用全局变量",
+    version: "0.0.1",
+  },
+  // Fix no parsing of HTML tags.
+  ".fff": {
+    preprocess: function(text, filename) {
+      const tpl = `<div>${text}</div>`;
+      return [tpl];
+    },
+    postprocess: function(messages, filename) {
+      return [].concat(...messages);
+    }
+  }
+};
