f272d475ed656758dcd8f8b51548ccb8b48d6638
zhaoshaoyong <zhaoshaoyong@360.cn>
2023-08-17 10:44:35 +0800

    add

---
+++ vue-project-demo/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, reactive, toRefs, toRef, isRef, watch } from 'vue'
+const count = ref(1)
+const obj = reactive({})
+const obj1 = reactive(count)
+// 装包
+const obj2 = ref(obj)
+console.log(obj2.value)
+// console.log(obj);
+// console.log(count.value);
+// 会拆包
+obj.count = count
+window.count = count
+window.obj = obj
+console.log(obj2) // 1
+console.log(obj.count === count.value) // true
+</script>
+
+<template>
+  <main>assas</main>
+</template>
